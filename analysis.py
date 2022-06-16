a7108cc88e6c2fb1dc1a222aa0a1763852356c22
doomed51 <doomed51@gmail.com>
2022-06-16 10:33:07 -0400

    modularized script, cleaned up draft code, updated commenting for clarity

--- analysis.py
+++ analysis.py
@@ -5,7 +5,10 @@
 
 from datetime import datetime
 from itertools import count
+from pyexpat import features
+from socket import gethostbyaddr
 from symtable import SymbolTableFactory
+from rich import print
 
 import ffn
 import sqlite3
@@ -15,24 +18,94 @@ from numpy import interp
 
 import pandas as pd
 import matplotlib.pyplot as plt
-matplotlib.use('TkAgg')
 import matplotlib.gridspec as gridspec
+import seaborn as sns
+
+matplotlib.use('TkAgg')
 
 """
-> Returns sesonal returns for the passed in OHLC timeseries
+global vars
+"""
+# symbols and timeframes to analyze
+symbols = ['RFV', 'IJJ', 'IVOV']
+intervals = ['yearByMonth', 'monthByDay', 'weekByDay', 'dayByHour']
+
+"""
+Returns a list of returns for a specific symbol, aggregated over  intervals
+----------
+1. connect to DB 
+2. iterate over each symbol, interval combo 
+    2.a. construct table name 
+    2.b. read OHLC data into DF 
+    2.c. compute returns for OHLC data
+    2.d. compute seasonal returns for the interval 
+3. append the aggregated seasonal returns
+4. return aggregated seasonal returns 
+
+"""
+def getSeasonalReturns(dbName = 'historicalData.db'):
+    conn = sqlite3.connect('historicalData.db')
+    seasonalReturns = []
+    for sym in symbols:
+        for int in intervals:
+            ## Tablename convention: <symbol>_<stock/opt>_<interval>
+            if int == 'yearByMonth':
+                tableName = sym+'_'+'stock'+'_'+'OneMonth'
+            
+            elif int == 'weekByDay':
+                tableName = sym+'_'+'stock'+'_'+'OneDay'
+            
+            elif int == 'dayByHour':
+                tableName = sym+'_'+'stock'+'_'+'OneHour'
+            
+            elif int == 'monthByDay':
+                tableName = sym+'_'+'stock'+'_'+'OneDay'
+
+            else:
+                tableName = sym+'_'+'stock'+'_'+int
+
+            sqlStatement = 'SELECT * FROM ' + tableName
+            symbolHistory = pd.read_sql(sqlStatement, conn)
+            myReturns = computeReturns(symbolHistory)
+            seasonalReturns.append(aggregateSeasonalReturns(myReturns, sym, int))
+
+    return seasonalReturns
+
+"""
+> Computes % return with OHLC data 
+
+Params
+---------
+symbolHistory - [dataframe] raw timeseries OHLC data for a symbol
+"""
+def computeReturns(symbolHist):
+    symbol = symbolHist['symbol'][1]
+    symbolHist = symbolHist[['start', 'close']]
+    symbolHist = symbolHist.set_index('start')
+    symbolHist.rename(columns={'close':symbol}, inplace=True)
+
+    ## compute returns data
+    returns = symbolHist.to_returns()
+
+    return returns
+
+
+"""
+> Aggregates 'returns' over specified time periods
 
 Params
 ----------
 returns - [dataframe] timeseries with returns column 
 """
-def getSeasonalReturns(returns, symbol, interval):
+def aggregateSeasonalReturns(returns, symbol, interval):
     symbol = returns.columns[0]
     
     returns = returns.reset_index()
     returns[['startDate', 'startTime']] = returns['start'].str.split("T", expand=True)
+    returns['startDate'] = pd.to_datetime(returns['startDate'])
     returns.drop(['start'], axis=1, inplace=True)
     
-    if interval in ['FiveMinutes', 'OneHour', 'FifteenMinutes']:
+    if interval in ['FiveMinutes', 'dayByHour', 'FifteenMinutes']:
 
         ## drop after and before hours data 
         returns = returns.loc[(returns['startTime'] >= "09:30:00") & (returns['startTime'] < "16:00:00")]
@@ -40,60 +113,38 @@ def getSeasonalReturns(returns, symbol, interval):
         # trim excess time info
         returns['startTime'] = returns['startTime'].astype(str).str[:-7]
 
-        ## Aggregate by start time
-        agg_hourlyReturns = returns.groupby('startTime').agg(
+        ## Aggregate by start time, computing mean and SD 
+        agg_myReturns = returns.groupby('startTime').agg(
             { symbol : ['mean', 'std']}
         )
 
-    elif interval in ['OneMonth']:
+    elif interval in ['yearByMonth']:
         # OneMonth - > month 
-        
-        returns['startDate'] = pd.to_datetime(returns['startDate'])
-        
-        agg_hourlyReturns = returns.groupby(returns['startDate'].dt.strftime('%B')).agg(
+        agg_myReturns = returns.groupby(returns['startDate'].dt.strftime('%m')).agg(
             { symbol : ['mean', 'std']}
         )
     
-    elif interval in ['OneDay']:
+    elif interval in ['weekByDay']:
         # OneDay -> day of the week
-        
-        returns['startDate'] = pd.to_datetime(returns['startDate'])
-        
-        agg_hourlyReturns = returns.groupby(returns['startDate'].dt.day_name()).agg(
+        agg_myReturns = returns.groupby(returns['startDate'].dt.day_name()).agg(
             { symbol : ['mean', 'std']}
         )
+    
+    elif interval == 'monthByDay':
+        # aggregate by startDate - > day value 
+        agg_myReturns = returns.groupby(returns['startDate'].dt.strftime('%d')).agg(
+            { symbol : ['mean', 'std']}
+        )
+
     
     # cleanup agg object 
-    agg_hourlyReturns.columns = agg_hourlyReturns.columns.get_level_values(1)
-    agg_hourlyReturns.reset_index(inplace=True)
-
-    agg_hourlyReturns['symbol'] = symbol
-    agg_hourlyReturns['interval'] = interval
+    agg_myReturns.columns = agg_myReturns.columns.get_level_values(1)
+    agg_myReturns.reset_index(inplace=True)
+    agg_myReturns['symbol'] = symbol
+    agg_myReturns['interval'] = interval
     
-    return agg_hourlyReturns
+    return agg_myReturns
 
-"""
-> Returns dataframe of % return from timeseries data 
-
-Params
----------
-symbolHistory - [dataframe] raw timeseries OHLC data for a symbol
-"""
-def computeReturns(symbolHist):
-    
-    ## parse history data into format that ffn expects 
-    ## i.e., [Date], [Price]
-    symbol = symbolHist['symbol'][1]
-
-    symbolHist = symbolHist[['start', 'close']]
-    symbolHist = symbolHist.set_index('start')
-
-    symbolHist.rename(columns={'close':symbol}, inplace=True)
-
-    ## compute returns data
-    returns = symbolHist.to_returns()
-
-    return returns
 
 """
 > plot seasonal returns 
@@ -104,90 +155,50 @@ seasonalReturns - [list] of [DataFrame] of seasonal returns
 intervals - [array] of intervals being plotted
 symbols - [array] of symbols being plotted
 """
-def plotSeasonalReturns(seasonalReturns, intervals, symbols):
+def plotSeasonalReturns(seasonalReturns):
     numCols = len(intervals)
     numRows = len(symbols)
     count = 0
 
     with plt.style.context(("seaborn","ggplot")):
-        fig = plt.figure(constrained_layout=True, figsize=(numCols*5, numRows*2))
+        
+        fig = plt.figure(constrained_layout=True, figsize=(numCols*5, numRows*3))
         specs = gridspec.GridSpec(ncols=numCols, nrows=numRows, figure=fig)
 
         for rtr in seasonalReturns:
             count += 1
             intervalLabel = rtr['interval'][0]
-            # labeling charts to be more intuitive 
-            if intervalLabel == 'OneMonth':
-                intervalLabel = 'Year by Month'
-            
-            elif intervalLabel == 'OneDay':
-                intervalLabel = 'Week by Day'
-            
-            elif intervalLabel == 'OneHour':
-                intervalLabel = 'Day by Hour'
 
             x1 = fig.add_subplot(numRows, numCols, count)
-            #x1.plot(rtr['startTime'], rtr['mean'], color='r', kind='bar')
             rtr['mean'].plot(color='r', kind='bar', title=rtr['symbol'][0]+' - '+intervalLabel, zorder=2)
 
             rtr['std'].plot(color='b', kind='bar')
-            #x1.plot(rtr['startTime'], rtr['std'], color='b', kind='bar')
+
 
         plt.show()
         plt.close(fig)
 
-"""
----------------------------------------------------------
-"""
-## declare vars
-#symbols = ['SLX', 'TIP', 'ERX', 'MOO', 'XLE', 'XME']
-symbols = ['SLX', 'FXY', 'TIP']
-intervals = ['OneMonth', 'OneDay', 'OneHour']
-seasonalReturns = list() #empty list to append history 
+def plotSeasonalReturns2(seasonalReturns):
+    numCols = len(intervals)
+    numRows = len(symbols)
+    count = 0
 
-## load data into a pandas dataframe 
-conn = sqlite3.connect('historicalData.db')
-for sym in symbols:
-    for int in intervals:
-        ## Tablename convention: <symbol>_<stock/opt>_<interval>
-        tableName = sym+'_'+'stock'+'_'+int
-        sqlStatement = 'SELECT * FROM ' + tableName
-        symbolHistory = pd.read_sql(sqlStatement, conn)
-        myReturns = computeReturns(symbolHistory)
-        seasonalReturns.append(getSeasonalReturns(myReturns, sym, int))
+    with plt.style.context(("seaborn","ggplot")):
+        
+        fig = plt.figure(constrained_layout=True, figsize=(numCols*5, numRows*3))
+        specs = gridspec.GridSpec(ncols=numCols, nrows=numRows, figure=fig)
 
-## get returns data for the symbol 
+        for rtr in seasonalReturns:
+            count += 1
+            intervalLabel = rtr['interval'][0]
 
-print(seasonalReturns[0:3])
+            x1 = fig.add_subplot(numRows, numCols, count)
+            
+            rtr['mean'].plot(color='r', kind='bar', title=rtr['symbol'][0]+' - '+intervalLabel, zorder=2)
+            rtr['std'].plot(color='b', kind='bar')
 
-plotSeasonalReturns(seasonalReturns, intervals, symbols)
 
-## compute seasonal returns
-#agg_hourlyReturns = getSeasonalReturns(myReturns)
+        plt.show()
+        plt.close(fig)
 
-""" print out resulting returns 
- 1 day = 78 lines of FiveMinutes data """
-
-"""
-
-print(agg_hourlyReturns[63:68])
-## plot the mean and std
-with plt.style.context(("seaborn","ggplot")):
-    plt.figure(figsize=(21,8))
-    agg_hourlyReturns['mean'].plot(kind='bar', color='r')
-    agg_hourlyReturns['std'].plot(kind='bar', color='b', zorder=-1)
-
-    plt.show()
-
-"""
-#-----------------------------------------------
-## ----------------graveyard--------------------
-#-----------------------------------------------
-#fig = plt.figure()
-#ax = returns.hist(figsize=(12, 5))
-#tb = returns.corr().as_format('.2f')
-
-#print(tb)
-#returns.plot_histogram()
-#plt.plot(ax)
-#plt.show()
+plotSeasonalReturns2(getSeasonalReturns())
