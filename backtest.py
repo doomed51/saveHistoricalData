ba8f2c80cdbc6455d11a5daa70e56f9627b12998
doomed51 <doomed51@gmail.com>
2022-07-05 10:33:32 -0400

    cleaned up seasonal strat template, added indicator strat template

--- backtest.py
+++ backtest.py
@@ -6,6 +6,10 @@ import backtrader as bt
 import backtrader.indicators as btind
 import pandas as pd
 
+## Default DB names 
+_dbName_stock = 'historicalData_stock.db'
+_dbName_index = 'historicalData_index.db'
+
 """
 ###########++++++++++++++++++++
 ########### UTIL FUNCTIONS 
@@ -246,7 +250,7 @@ class SMA_CrossOver(bt.Strategy):
             self.buy()
 
 
-class SLX_seasonal(bt.Strategy):
+class seasonal(bt.Strategy):
     params = (
         ('smaperiod', 20),
         ('printlog', False),
@@ -268,9 +272,12 @@ class SLX_seasonal(bt.Strategy):
         self.buyprice = None
         self.buycomm = None
 
-        ## Add a MovingAverageSimple indicator
+        ## Add a macd indicator
         self.macd = bt.indicators.MACD(
             self.datas[0])
+
+        # add RSI indicator 
+        self.rsi = bt.indicators.RSI(self.datas[0])
         
     def notify_order(self, order):
         if order.status in [order.Submitted, order.Accepted]:
@@ -314,18 +321,114 @@ class SLX_seasonal(bt.Strategy):
         #self.log('Close, %.2f, VWAP: %.2f'%(self.dataclose[0], self.vwap[0]))
 
         # Check if an order is pending ... if yes, we cannot send a 2nd one
-        #dateToGoLong = datetime.datetime.strptime("01-07")
-        #dateToGoShort = 
-
         if self.order:
             return
 
         # Check if we are in the market
         if not self.position:
             mydate = self.datetime.date()
-            if (mydate.month == 10):
-                if ( 7 <= mydate.day <= 12 ):
-                    #print ('BUY!!!!!!!!!')
+            if (mydate.month == 4):
+                #if (self.rsi <= 30):
+                # BUY with default parameters
+                self.log('BUY Open, %.2f' % self.dataclose[0])
+
+                # Keep track of the created order to avoid a 2nd order
+                self.order = self.buy()
+
+        else:
+            mydate = self.datetime.date()
+            if (6<=mydate.month<=9):
+                #if (self.rsi >= 70):
+                # SELL with all possible default parameters
+                self.log('SELL Open, %.2f' % self.dataclose[0])
+
+                # Keep track of the created order to avoid a 2nd order
+                self.order = self.sell()
+                
+
+    def stop(self):
+        self.log('(MA Period %2d) Ending Value %.2f'%(self.params.smaperiod, self.broker.getvalue()), doprint=True)
+
+class basicMomentum(bt.Strategy):
+    params = (
+        ('smaperiod', 20),
+        ('printlog', False),
+        ('vwapperiod', 12)
+    )
+    
+    def log(self, txt, dt=None, doprint=True):
+        ''' Logging function for this strategy'''
+        if self.params.printlog or doprint:
+            dt = dt or self.datas[0].datetime.date(0)
+            print('%s, %s' % (dt.isoformat(), txt))
+    
+    def __init__(self):
+        ## keep a reference to the close price in the data[0] dataseries
+        self.dataclose = self.datas[0].close
+
+        ## to keep track of pending orders
+        self.order = None
+        self.buyprice = None
+        self.buycomm = None
+
+        ## Add a macd indicator
+        self.macd = bt.indicators.MACD(
+            self.datas[0])
+
+        # add RSI indicator 
+        self.rsi = bt.indicators.RSI(self.datas[0])
+        
+    def notify_order(self, order):
+        if order.status in [order.Submitted, order.Accepted]:
+            # Buy/Sell order submitted/accepted to/by broker - Nothing to do
+            return
+
+        # Check if an order has been completed
+        # Attention: broker could reject order if not enough cash
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                self.log(
+                    'BUY EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
+                    (order.executed.price,
+                     order.executed.value,
+                     order.executed.comm))
+
+                self.buyprice = order.executed.price
+                self.buycomm = order.executed.comm
+            else:  # Sell
+                self.log('SELL EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
+                         (order.executed.price,
+                          order.executed.value,
+                          order.executed.comm))
+
+            self.bar_executed = len(self)
+
+        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+            self.log('Order Canceled/Margin/Rejected')
+
+        self.order = None
+
+    def notify_trade(self, trade):
+        if not trade.isclosed:
+            return
+
+        self.log('OPERATION PROFIT, GROSS %.2f, NET %.2f' %
+                 (trade.pnl, trade.pnlcomm))
+
+    def next(self):
+        # log the closing price
+        #self.log('Close, %.2f, VWAP: %.2f'%(self.dataclose[0], self.vwap[0]))
+
+        # Check if an order is pending ... if yes, we cannot send a 2nd one
+        if self.order:
+            return
+
+        # Check if we are in the market
+        if not self.position:
+            mydate = self.datetime.date()
+            if (mydate.month == 4 or mydate.month == 7):
+                #if ( 1 <= mydate.day <= 20 ):
+                if (self.rsi >= 80):
                     # SELL with all possible default parameters
                     self.log('SELL Open, %.2f' % self.dataclose[0])
 
@@ -334,8 +437,9 @@ class SLX_seasonal(bt.Strategy):
 
         else:
             mydate = self.datetime.date()
-            if (mydate.month == 12):
-                if ( 24 <= mydate.day ):
+            if (mydate.month == 6 or 8 <= mydate.month <= 9):
+                #if ( 5 <= mydate.day ):
+                if (self.rsi <= 50):
                     # BUY with default parameters
                     self.log('BUY Open, %.2f' % self.dataclose[0])
 
@@ -344,7 +448,6 @@ class SLX_seasonal(bt.Strategy):
 
     def stop(self):
         self.log('(MA Period %2d) Ending Value %.2f'%(self.params.smaperiod, self.broker.getvalue()), doprint=True)
-
 """
 ##########
 ########## STRATEGIES /END   
@@ -355,16 +458,16 @@ class SLX_seasonal(bt.Strategy):
 cr = bt.Cerebro()
 
 # Add the Data Feed to Cerebro
-cr.adddata(getHistory_SQL('historicalData.db', 'UNG', 'OneDay'))
+cr.adddata(getHistory_SQL(_dbName_stock, 'XLE', 'OneDay'))
 
 ## add a strategy
-cr.addstrategy(SLX_seasonal)
+cr.addstrategy(seasonal)
 
 ## set cash 
-cr.broker.setcash(2000)
+cr.broker.setcash(10000)
 
 # Add a FixedSize sizer according to the stake
-cr.addsizer(bt.sizers.FixedSize, stake=2)
+cr.addsizer(bt.sizers.FixedSize, stake=50)
 
 # Set the commission - 0.1% ... divide by 100 to remove the %
 cr.broker.setcommission(commission=0.01)
