eeceb622fe777609c8411f9c130281cc820e71bf
doomed51 <doomed51@gmail.com>
2023-03-07 18:22:50 -0500

    added update function for lookup table

--- localDbInterface.py
+++ localDbInterface.py
@@ -1,11 +1,10 @@
 """
-This module simplifies interacting with the local database of stock and ticker data. It consolidates functionality the previously existed across mutliple in the system. 
+This module simplifies interacting with the local database of security historical data. 
 
-checklist: 
-Create - 0
-Read - 0
-Update - 0
-Delete - 0
+    - connect to db
+    - save historical data to local db 
+    - retrieve historical data for symbol and interval 
+    - automatically clears duplicates if any
 
 """
 
@@ -27,7 +26,96 @@ intervalMappings = pd.DataFrame(
     }
 )
 
-## TODO test the below function, imported from another module...
+"""
+Establishes a connection to the appropriate DB based on type of symbol passed in. 
+
+Returns sqlite connection object 
+
+"""
+def _connectToDb():
+    return sqlite3.connect(dbname_index)
+
+"""
+constructs the appropriate tablename to call local DB 
+
+Params
+===========
+symbol - [str]
+interval - [str] (must match with intervalMappings global var)
+
+"""
+def _constructTableName(symbol, interval):
+    type_ = 'stock'
+    if symbol.upper() in index_list:
+        type_ = 'index'
+
+    tableName = symbol+'_'+type_+'_'+intervalMappings.loc[intervalMappings['label'] == interval][type_].values[0]
+
+    return tableName
+
+"""
+utility - permanently remove duplicate records from ohlc table
+
+Params
+==========
+tablename - [str]
+"""
+def _removeDuplicates(tablename):
+    conn = _connectToDb() # connect to DB
+
+    ## construct SQL qeury that will group on 'date' column and
+    ## select the min row ID of each group; then delete all the ROWIDs from 
+    ## the table that not in this list
+    sql_selectMinId = 'DELETE FROM %s WHERE ROWID NOT IN (SELECT MIN(ROWID) FROM %s GROUP BY date)'%(tablename, tablename)
+
+    ## run the query 
+    cursor = conn.cursor()
+    cursor.execute(sql_selectMinId)
+
+"""
+Utility to update the symbol record lookup table
+This should be called when local db records are updated 
+This should not be run before security history is added to the db 
+
+Params
+----------
+tablename: table that needs to be updated 
+numMissingDays: number of days we do not have locally  
+"""
+def _updateLookup_symbolRecords(conn, tablename, numMissingDays = 5, earliestTimestamp = ''):
+    lookupTablename = '00-lookup_symbolRecords'
+    
+    ## get the earliest record date saved for the target symbol 
+    sql_minDate_symbolHistory = 'SELECT MIN(date), symbol, interval FROM %s'%(tablename)
+    minDate_symbolHistory = pd.read_sql(sql_minDate_symbolHistory, conn)
+    
+    ## get the earliest date from the lookup table for the matching symbol 
+    sql_minDate_recordsTable = 'SELECT earliestRecordDate FROM \'%s\' WHERE symbol = \'%s\' and interval = \'%s\''%('00-lookup_symbolRecords', minDate_symbolHistory['symbol'][0], minDate_symbolHistory['interval'][0])
+    minDate_recordsTable = pd.read_sql(sql_minDate_recordsTable, conn)
+    
+    ## add a new record entry in the lookup table since none are there 
+    if minDate_recordsTable.empty:
+        ## compute the number of missing business days 
+        ## since this is a new record, we expect a timestamp to have been passed on the call to write history to the db 
+        if not earliestTimestamp:
+            ## set missing business days to the difference between the earliest available date in ibkr and the earliest date in the local db  
+            print(minDate_symbolHistory.iloc[0]['MIN(date)'])
+            print(earliestTimestamp)
+            defaultNumMissingDays = len(pd.bdate_range(earliestTimestamp, minDate_symbolHistory.iloc[0]['MIN(date)']))
+
+        ## add missing columns 
+        minDate_symbolHistory['numMissingBusinessDays'] = numMissingDays
+        minDate_symbolHistory['name'] = tablename
+        
+        ## rename columns to match db table columns 
+        minDate_symbolHistory.rename(columns={'MIN(date)':'earliestRecordDate'}, inplace=True)
+        minDate_symbolHistory = minDate_symbolHistory.iloc[:,[4,1,2,0,3]]
+      
+        ## save record to db
+        minDate_symbolHistory.to_sql(f"{lookupTablename}", conn, index=False, if_exists='append')
+    
+    ## otherwise update the existing record 
+
 """
 Save history to a sqlite3 database
 ###
@@ -35,11 +123,11 @@ Save history to a sqlite3 database
 Params
 ------------
 history: [DataFrame]
-    pandas dataframe with date and OHLC, volume, interval, and vwap  
+    pandas dataframe with security timeseries data
 conn: [Sqlite3 connection object]
     connection to the local db 
 """
-def saveHistoryToDB(history, conn):
+def saveHistoryToDB(history, conn, earliestTimestamp=''):
     
     ## set type to index if the symbol is in the index list 
     if history['symbol'][0] in index_list:
@@ -47,12 +135,15 @@ def saveHistoryToDB(history, conn):
     else: 
         type='stock'
     
-    # Write the dataframe to the database with the correct table name
+    # Write the dataframe to the database with the correctly formatted table name
     tableName = history['symbol'][0]+'_'+type+'_'+history['interval'][0]
     history.to_sql(f"{tableName}", conn, index=False, if_exists='append')
     
     #make sure there are no duplicates in the resulting table
     _removeDuplicates(tableName)
+
+    ## make sure the records lookup table is kept updated
+    _updateLookup_symbolRecords(conn, tableName, earliestTimestamp=earliestTimestamp)
 
     ## Update the symbolRecord lookup table 
     # table name: 00-lookup_symbolRecords
@@ -77,7 +168,7 @@ lookback - [str] optional
 """
 def getPriceHistory(symbol, interval):
     tableName = _constructTableName(symbol, interval)
-    conn = _connectToDb(symbol)
+    conn = _connectToDb()
     sqlStatement = 'SELECT * FROM '+tableName
     pxHistory = pd.read_sql(sqlStatement, conn)
     conn.close()
@@ -87,54 +178,15 @@ def getPriceHistory(symbol, interval):
     
     return pxHistory
 
+
+""" 
+Returns the lookup table fo records history as df 
 """
-establishes a connection to the appropriate DB based on type of symbol passed in. 
+def getLookup_symbolRecords():
+    conn = _connectToDb()
 
-Returns sqlite connection object 
+    sqlStatement_selectRecordsTable = 'SELECT * FROM \'00-lookup_symbolRecords\''
 
-Params
-========
-symbol - [str] 
-"""
-def _connectToDb():
-    return sqlite3.connect(dbname_index)
+    symbolRecords = pd.read_sql(sqlStatement_selectRecordsTable, conn)
 
-"""
-constructs the appropriate tablename to call local DB 
-
-Params
-===========
-symbol - [str]
-interval - [str] (must match with intervalMappings global var)
-
-"""
-def _constructTableName(symbol, interval):
-    type_ = 'stock'
-    if symbol.upper() in index_list:
-        type_ = 'index'
-
-    tableName = symbol+'_'+type_+'_'+intervalMappings.loc[intervalMappings['label'] == interval][type_].values[0]
-
-    return tableName
-
-"""
-utility - remove duplicate records from ohlc table
-
-Params
-==========
-tablename - [str]
-"""
-def _removeDuplicates(tablename):
-    conn = _connectToDb() # connect to DB
-
-    ## group on date & select the min row IDs; then delete all the ROWIDs not in the selected list
-    sql_selectMinId = 'DELETE FROM %s WHERE ROWID NOT IN (SELECT MIN(ROWID) FROM %s GROUP BY date)'%(tablename, tablename)
-
-    ## run the query 
-    cursor = conn.cursor()
-    cursor.execute(sql_selectMinId)
-
-
-_removeDuplicates('VIX_index_5mins')
-
-
+    return symbolRecords
